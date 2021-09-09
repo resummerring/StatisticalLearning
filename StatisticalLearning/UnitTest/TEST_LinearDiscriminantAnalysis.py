b8920055ccd2d566ad35410e4993c2a5b46d6fad
resummerring <15300666575@163.com>
2021-09-09 00:03:37 -0400

    added dimension reduction technique for linear discriminant analysis and multi-class unit test

--- StatisticalLearning/UnitTest/TEST_LinearDiscriminantAnalysis.py
+++ StatisticalLearning/UnitTest/TEST_LinearDiscriminantAnalysis.py
@@ -2,7 +2,7 @@ import unittest
 import warnings
 import pandas as pd
 from sklearn import datasets
-from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from StatisticalLearning.LinearModel.Classification import LinearDiscriminantAnalysis
 
 
@@ -16,11 +16,36 @@ class TEST_LinearDiscriminantAnalysis(unittest.TestCase):
         X, y = datasets.load_breast_cancer(return_X_y=True)
         X, y = pd.DataFrame(X), pd.Series(y)
 
-        lr = LogisticRegression(fit_intercept=True).fit(X, y)
-        lda = LinearDiscriminantAnalysis().fit(X, y)
+        lda = LinearDiscriminantAnalysis(n_components=5).fit(X, y)
+        sklearn_lda = LDA().fit(X, y)
 
-        pred_lr = lr.predict(X)
         pred_lda = lda.predict(X)
-        self.assertTrue((pred_lda - pd.Series(pred_lr.squeeze())).abs().sum() <= 30)
+        pred_sklearn = sklearn_lda.predict(X)
+
+        self.assertTrue((pred_lda - pd.Series(pred_sklearn.squeeze())).abs().sum() == 0)
+
+    def test_multiple_classification(self):
+
+        X, y = datasets.load_digits(return_X_y=True)
+        X, y = pd.DataFrame(X), pd.Series(y)
+
+        lda = LinearDiscriminantAnalysis(n_components=5).fit(X, y)
+        sklearn_lda = LDA().fit(X, y)
+
+        pred_lda = lda.predict(X)
+        pred_sklearn = sklearn_lda.predict(X)
+
+        self.assertTrue((pred_lda - pd.Series(pred_sklearn.squeeze())).abs().sum() == 0)
+
+    def test_transform(self):
+
+        # TODO: Compare discriminant direction with sklearn
+        X, y = datasets.load_digits(return_X_y=True)
+        X, y = pd.DataFrame(X), pd.Series(y)
+
+        lda = LinearDiscriminantAnalysis(n_components=5).fit(X, y)
+        X_lda_tran = lda.transform(X.copy(deep=True)).applymap(lambda num: num.real)
+
+        self.assertTrue(X_lda_tran.shape[0] == X.shape[0] and X_lda_tran.shape[1] == 5)
 
 
