5d3db4d38e80e9ee7f9c63144f150af162895b89
resummerring <15300666575@163.com>
2021-10-13 00:56:55 -0400

    added alpha vector and alpha factor

--- StatisticalLearning/FactorAnalysis/Factor.py
+++ StatisticalLearning/FactorAnalysis/Factor.py
@@ -2,26 +2,28 @@ from __future__ import annotations
 
 import numpy as np
 import pandas as pd
-from typing import Union
+from typing import Union, List, Any
 
 
-class Factor:
+class AlphaVector:
 
     """
-    A factor is a numeric value, one for each stock, potentially predictive of performance
+    An alpha vector is a vector of alpha values at a single time period over stock universe
 
     Alpha factor: drivers of mean return
     Risk factor: drivers of volatility
     """
 
-    def __init__(self, factor: pd.Series):
+    def __init__(self, vector: pd.Series, index: Any):
         """
-        :param factor: pd.Series, index is stock tick and value is alpha value
+        :param vector: pd.Series, index is stock tick and value is alpha value
+        :param index: Any, an indicator for the time period of alpha vector with <,>,= overriden
         """
 
-        self._factor = factor
+        self._vector = vector
+        self._index = index
 
-    def market_neutral(self) -> Factor:
+    def market_neutral(self) -> AlphaVector:
         """
         x(i) = x(i) - mean_i x(i) -> mean_i x(i) = 0
 
@@ -29,10 +31,10 @@ class Factor:
         excluding the influence from overall market when testing the predictive power of the factor.
         """
 
-        self._factor = self._factor - self._factor.mean(skipna=True)
+        self._vector = self._vector - self._vector.mean(skipna=True)
         return self
 
-    def leverage_neutral(self) -> Factor:
+    def leverage_neutral(self) -> AlphaVector:
         """
         x(i) = x(i) / sum_i |x(i)| -> sum_i |x(i)| = 1
 
@@ -40,10 +42,10 @@ class Factor:
         Make different factors more comparable to each other by forcing same amount of dollar.
         """
 
-        self._factor = self._factor / self._factor.abs().sum(skipna=True)
+        self._vector = self._vector / self._vector.abs().sum(skipna=True)
         return self
 
-    def re_scale(self) -> Factor:
+    def re_scale(self) -> AlphaVector:
         """
         x(i) = x(i) / std_i x(i) -> std_i x(i) = 1
 
@@ -51,10 +53,10 @@ class Factor:
         more comparable to each other.
         """
 
-        self._factor = self._factor / np.std(self._factor, ddof=1)
+        self._vector = self._vector / np.std(self._vector, ddof=1)
         return self
 
-    def sector_neutral(self, sector_map: dict) -> Factor:
+    def sector_neutral(self, sector_map: dict) -> AlphaVector:
         """
         :param sector_map: dict, a mapping from stock tick to corresponding sector
 
@@ -63,20 +65,20 @@ class Factor:
         """
 
         factor = pd.DataFrame(columns=['Return', 'Sector'])
-        factor['Return'] = self._factor
+        factor['Return'] = self._vector
         factor['Sector'] = factor['Return'].apply(lambda tick: sector_map[tick])
 
         sector_mean = factor.groupby('Sector')['Return'].mean()
         factor['Return'] = factor.apply(lambda row: row['Return'] - sector_mean[row['Sector']])
 
-        self._factor = factor['Return']
+        self._vector = factor['Return']
 
         return self
 
     def winsorize(self,
                   cutoff: float = 0.9,
                   hard_lower: Union[None, float] = None,
-                  hard_upper: Union[None, float] = None) -> Factor:
+                  hard_upper: Union[None, float] = None) -> AlphaVector:
         """
         Remove outlier outsize of [cutoff, 1 - cutoff] of the factor values
 
@@ -85,28 +87,82 @@ class Factor:
         :param hard_upper: Union[None, float], hard upper threshold to apply if given
         """
 
-        q_lower = self._factor.quantile(q=cutoff, interpolation='linear')
-        q_upper = self._factor.quantile(q=1-cutoff, interpolation='linear')
+        q_lower = self._vector.quantile(q=cutoff, interpolation='linear')
+        q_upper = self._vector.quantile(q=1 - cutoff, interpolation='linear')
 
         q_lower = q_lower if hard_lower is None else hard_lower
         q_upper = q_upper if hard_upper is None else hard_upper
 
-        self._factor[self._factor < q_lower] = q_lower
-        self._factor[self._factor > q_upper] = q_upper
+        self._vector[self._vector < q_lower] = q_lower
+        self._vector[self._vector > q_upper] = q_upper
 
         return self
 
-    def rank(self) -> Factor:
+    def rank(self) -> AlphaVector:
         """
         Transform alpha signal to be less sensitive to noise and outliers and avoid excessive trades
+        Cannot make alpha signal comparable if generated from different stock universe
         """
 
-        self._factor = self._factor.rank(axis=0, method='first', na_option='keep', ascending=True)
+        self._vector = self._vector.rank(axis=0, method='first', na_option='keep', ascending=True)
         return self
+
+    def z_score(self) -> AlphaVector:
+        """
+        Standardize to zero mean and unit variance
+        Useful when alpha signals are generated from different stock universe
+        """
+
+        return self.market_neutral().re_scale()
 
     @property
     def factor(self) -> pd.Series:
         """
-        Return factor time series
+        Return alpha value vector
         """
-        return self._factor
+        return self._vector
+
+    @property
+    def index(self) -> Any:
+        """
+        Return vector time period
+        """
+        return self._index
+
+
+class AlphaFactor:
+
+    def __init__(self, vectors: List[AlphaVector]):
+        """
+        :param vectors: List[AlphaVector], a collection of alpha factors from different time periods
+
+        Assume all alpha vector comes from the same stock universe
+        """
+
+        vectors.sort(key=lambda vector: vector.index, reverse=False)
+
+        columns = list(vectors[0].index)
+        index = [vector.index for vector in vectors]
+
+        self._factor = pd.DataFrame(columns=columns, index=index)
+        for i, vector in enumerate(vectors):
+            self._factor.iloc[i, :] = np.array(vector.factor)
+
+    def smooth(self, columns: Union[List[str], None], weight: List[float]) -> AlphaFactor:
+        """
+        Smooth alpha factors are by using weighted moving average
+
+        :param columns: Union[List[str], None], subset of columns to apply moving average
+        :param weight: List[float], weight to use for the rolling window
+        """
+
+        if columns is None:
+            self._factor = self._factor.apply(
+                lambda col: col.rolling.mean(window=len(weight))
+                .apply(lambda window: np.dot(window, weight) / sum(weight)), axis=0)
+        else:
+            self._factor[columns] = self._factor[columns].apply(
+                lambda col: col.rolling.mean(window=len(weight))
+                .apply(lambda window: np.dot(window, weight) / sum(weight)), axis=0)
+
+        return self
