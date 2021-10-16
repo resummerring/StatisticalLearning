4cbc6176d8321cee4c6a660c5df5fb7eb0bfed5b
resummerring <15300666575@163.com>
2021-10-15 21:53:22 -0400

    completed univariate evaluation class

--- StatisticalLearning/FactorAnalysis/AlphaEvaluation.py
+++ StatisticalLearning/FactorAnalysis/AlphaEvaluation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from typing import Callable
-from StatisticalLearning.FactorAnalysis.AlphaFactor import AlphaFactor
+from StatisticalLearning.FactorAnalysis.AlphaFactor import AlphaFactor, AlphaVector
 
 
 class UniAlphaEval:
@@ -47,7 +47,7 @@ class UniAlphaEval:
         sharpe_ratio = forward_return.mean() / forward_return.std()
         return sharpe_ratio * np.sqrt(252) / np.sqrt(frequency)
 
-    def rank_IC(self) -> pd.Series:
+    def rank_info_ratio(self) -> pd.Series:
         """
         Calculate portfolio rank information coefficient = Corr(Rank(alpha(t-1)), Rank(p(t)))
         Rank IC is more robust than pearson correlation since we care more about the right
@@ -63,3 +63,65 @@ class UniAlphaEval:
         rank_corr = [np.corrcoef(alpha_rank.iloc[i, :], return_rank.iloc[i, :]) for i in range(alpha_rank.shape[0])]
         return pd.Series(rank_corr, index=self._stock_return.index)
 
+    def rank_auto_corr(self) -> pd.Series:
+        """
+        Calculate factor rank auto-correlation(t) = Corr(Rank(alpha(t-1)), Rank(alpha(t)))
+        Higher rank auto-correlation -> lower turnover -> low transaction cost + liquidity
+        """
+
+        alpha_rank = self._factor.alpha.apply(
+            lambda row: row.rank(axis=0, method='first', na_option='keep', ascending=True), axis=1)
+
+        autocorr = []
+        for i in range(alpha_rank.shape[0] - 1):
+            last_alpha, current_alpha = alpha_rank.iloc[i, :], alpha_rank.iloc[i + 1, :]
+            autocorr.append(np.corrcoef(last_alpha, current_alpha))
+
+        return pd.Series(autocorr, index=alpha_rank.index.values.tolist()[1:])
+
+    def turnover(self):
+        """
+        Calculate portfolio turnover(t) = sum_i [weight(t, i) - weight(t - 1, i)]
+        High turn over -> high transaction cost
+        """
+
+        weight = self._factor.weight(transformer=self._transformer)
+
+        turn_over = []
+        for i in range(weight.shape[0] - 1):
+            last_weight, current_weight = weight.iloc[i, :], weight.iloc[i + 1, :]
+            turn_over.append(np.sum(np.abs(last_weight - current_weight)))
+
+        return pd.Series(turn_over, index=weight.idnex.values.tolist()[1:])
+
+    def transfer_coef(self, optimal_weights: pd.DataFrame):
+        """
+        Calculate transfer coefficient between alpha vector and optimized portfolio weight.
+        The higher the correlation, the more influence alpha vector takes into the portfolio
+        and the less likely risk factor and alpha factors are correlated.
+
+        :param optimal_weights: pd.Series, index are time periods, columns are stock universe,
+        values are stock weights in optimized portfolio
+        """
+
+        alpha = self._factor.weight(transformer=self._transformer)
+
+        assert optimal_weights.shape == alpha.shape, f"Dimension mismatch: portfolio " \
+            f"weight with shape {optimal_weights.shape} while alpha factor with shape {alpha.shape}"
+
+        coef = []
+        for i in range(optimal_weights.shape[0]):
+            coef.append(np.corrcoef(optimal_weights.iloc[i, :], alpha.iloc[i, :]))
+
+        return pd.Series(coef, index=alpha.index)
+
+    def conditional_alpha(self, factor: AlphaFactor) -> AlphaFactor:
+
+        assert self._factor.alpha.index == factor.alpha.index, "Time period mismatch between alpha factors"
+
+        combined_alpha, index = [], factor.alpha.index.values.tolist()
+        for i in range(factor.alpha.shape[0]):
+            combined_alpha.append(
+                AlphaVector(vector=self._factor.alpha.iloc[i, :].mul(factor.alpha.iloc[i, :]), index=index[i]))
+
+        return AlphaFactor(combined_alpha)
