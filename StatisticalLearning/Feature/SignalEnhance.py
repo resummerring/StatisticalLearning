521d7643e8538c8395b0ba9659896304d465c374
resummerring <15300666575@163.com>
2021-10-18 22:03:03 -0400

    added denoise technique and unittest with covariance with signals

--- StatisticalLearning/Feature/SignalEnhance.py
+++ StatisticalLearning/Feature/SignalEnhance.py
@@ -18,22 +18,42 @@ class SignalEnhance:
     between theoretical pdf value and fitted KDE pdf values. With fitted sigma and max eigenvalue limit, we can
     differentiate noises from signals. Fitted sigma^2 can be regarded as the proportion of variance that can be
     explained by the noise (randomness) of the data matrix.
+
+    After the signal and noise components are identified, denoising and detoning techniques can be used to process
+    the correlation matrix and generated a more robust, well-conditioned, well-informed correlation matrix.
     """
 
-    def __init__(self, matrix: np.ndarray, band_width: float = 0.25, kernel: str = 'gaussian', n_steps: int = 1000):
+    def __init__(self, matrix: np.ndarray, q: Union[float, None] = None, is_cov: bool = False,
+                 band_width: float = 0.25, kernel: str = 'gaussian', n_steps: int = 1000):
 
         """
-        :param matrix: np.ndarray, T * N observation matrix with T > N
+        :param matrix: np.ndarray, T * N observation matrix with T > N if is_cov = False else N * N covariance matrix
+        :param q: Union[float, None], if is_cov = true, q must be provided, otherwise q will be deduced from matrix
+        :param is_cov, bool, if true then covariance matrix is provided else observation matrix is provided
         :param band_width: float, band width for fitting kernel density, by default 0.25
         :param kernel: str, distribution for fitting kernel density, by default gaussian
         :param n_steps: int, number of discrete pdf values to evaluate for optimization
         """
 
-        self._matrix = matrix
+        # Observation matrix property
+        self._matrix = matrix if not is_cov else None
+        self._q = self._matrix.shape[0] / self._matrix.shape[1] if not is_cov else q
+
+        if self._q is None:
+            raise ValueError("If covariance matrix is provided, q is also needed")
+
+        # Original covariance / correlation matrix property
+        self._original_cov = np.cov(self._matrix, rowvar=False) if not is_cov else matrix
+        self._original_corr = self.cov_to_corr(self._original_cov)
+        self._original_corr_eval, self._original_corr_evec = self.get_sorted_pca(self._original_corr)
+
+        # Denoised correlation matrix property
+        self._denoised_corr = None
+
+        # Hyper-parameter tuning KDE
         self._band_width = band_width
         self._kernel = kernel
         self._n_steps = n_steps
-        self._q = self._matrix.shape[0] / self._matrix.shape[1]
 
     # ====================
     #  Private
@@ -78,17 +98,19 @@ class SignalEnhance:
         pdf_kde = self._fit_empirical_kde(lambds=lambds, val=pdf_mp.index.values)
         return np.sum(np.square(pdf_mp - pdf_kde)).squeeze()
 
-    def _fit_max_eval(self, lambds: np.ndarray) -> Tuple[float, float]:
+    def _fit_max_eval(self) -> Tuple[float, float]:
         """
         Calibrate covariance matrix eigenvalues towards Marcenko-Pastur distribution to get max eigenvalue and sigma
-
-        :param lambds: pd.Series, 1D points on which kernel density will be fitted
         """
+
+        lambds = np.diag(self._original_corr_eval)
+
         minimizer = minimize(lambda *x: self._pdf_error(*x), x0=np.array([0.5]),
                              args=(lambds,), bounds=((1e-5, 1 - 1e-5),))
 
         sigma = minimizer['x'][0] if minimizer['success'] else 1
         eval_max = np.sqrt(sigma) * np.square(1 + 1. / np.sqrt(self._q))
+
         return eval_max, sigma
 
     # ====================
@@ -126,14 +148,54 @@ class SignalEnhance:
 
         return corr
 
-    def calibrate(self) -> Tuple[float, float]:
+    def denoise(self, method: str = 'Constant Residual', alpha: Union[float, None] = 1.):
         """
-        Main function to calibrate observation matrix to find optimal sigma
+        Denoise correlation matrix according to calibrated sigma and max eigenvalue threshold:
+        Assume {eval} i = 1,...,N are the descendingly sorted eigenvalues from correlation matrix,
+        find k such that eval(k) > eval_max -> signal and eval(k+1) <= eval_max -> noise
+
+        1. Constant Residual:
+            (1) set all {eval(i) | eval(i) < eval_max} = mean({eval(i) | eval(i) < eval_max}) -> {new_eval}
+            (2) denoised correlation = rescaled(evec @ eval_new @ evec.T)
+
+            Trace is preserved, minimum eval is increased -> condition number is improved
+
+        2. Target Shrinkage:
+            (1) signal_component = evec_signal @ eval_signal @ evec_signal.T
+            (2) noise_component = evec_noise @ eval_noise @ evec_noise.T
+            (3) denoised correlation = signal_component + alpha * noise_component + (1 - alpha) * diag(noise_component)
+
+            alpha = 1 -> no shrinkage, alpha = 0 -> total shrinkage
+
+        :param method: str, shrinkage method {'Constant Residual', 'Target Shrinkage'}
+        :param alpha: Union[float, None], only used with Target Shrinkage, 1 = no shrinkage and 0 = total shrinkage
         """
 
-        cov = np.cov(self._matrix, rowvar=False)
-        corr = self.cov_to_corr(cov)
-        eval, evec = self.get_sorted_pca(corr)
-        e_max, sigma = self._fit_max_eval(np.diag(eval))
+        eval_max, sigma = self._fit_max_eval()
+        lambds = np.diag(self._original_corr_eval).copy()
 
-        return e_max, sigma
+        n_factors = next(index for index, val in enumerate(lambds) if val < eval_max)
+
+        if method == 'Constant Residual':
+
+            lambds[n_factors:] = lambds[n_factors:].sum() / float(lambds.shape[0] - n_factors)
+            denoised_corr_eval = np.diag(lambds)
+            denoised_corr = np.dot(self._original_corr_evec, denoised_corr_eval).dot(self._original_corr_evec.T)
+
+            # Rescale diagonal element to be 1
+            self._denoised_corr = self.cov_to_corr(denoised_corr)
+
+        elif method == 'Target Shrinkage':
+
+            lambds_signal = self._original_corr_eval[:n_factors, :n_factors]
+            evec_signal = self._original_corr_evec[:, :n_factors]
+            lambds_noise = self._original_corr_eval[n_factors:, n_factors:]
+            evec_noise = self._original_corr_evec[:, n_factors:]
+
+            if not lambds_signal.shape[0] == 0:
+                corr_signal = np.dot(evec_signal, lambds_signal).dot(evec_signal.T)
+                corr_noise = np.dot(evec_noise, lambds_noise).dot(evec_noise.T)
+                self._denoised_corr = corr_signal + alpha * corr_noise + (1 - alpha) * np.diag(np.diag(corr_noise))
+
+        else:
+            raise ValueError("Accepted shrinkage methods are: (1) Constant Residual (2) Target Shrinkage")
--- StatisticalLearning/UnitTest/TEST_SignalEnhance.py
+++ StatisticalLearning/UnitTest/TEST_SignalEnhance.py
@@ -10,14 +10,26 @@ sns.set_style('darkgrid')
 class TEST_SignalEnhance(unittest.TestCase):
 
     def setUp(self):
-        self._random_matrix = np.random.normal(size=(10000, 1000))
-        self._enhancer = SignalEnhance(matrix=self._random_matrix, band_width=0.01)
 
-    def test_marcenko_pastur_pdf_against_kde_fit(self):
+        def get_random_cov(n_cols: int, n_factors: int):
+            """
+            Generate random covariance matrix
+            """
 
-        eval, evec = self._enhancer.get_sorted_pca(np.corrcoef(self._random_matrix, rowvar=False))
-        pdf_mp = self._enhancer._marcenko_pastur_pdf(sigma=1)
-        pdf_kde = self._enhancer._fit_empirical_kde(lambds=np.diag(eval))
+            w = np.random.normal(size=(n_cols, n_factors))
+            cov = np.dot(w, w.T) + np.diag(np.random.uniform(size=n_cols))
+            return cov
+
+        self._get_random_cov = get_random_cov
+
+    def test_random_matrix(self):
+
+        random_matrix = np.random.normal(size=(10000, 1000))
+        enhancer = SignalEnhance(matrix=random_matrix, band_width=0.01)
+
+        eval, evec = enhancer.get_sorted_pca(np.corrcoef(random_matrix, rowvar=False))
+        pdf_mp = enhancer._marcenko_pastur_pdf(sigma=1)
+        pdf_kde = enhancer._fit_empirical_kde(lambds=np.diag(eval))
 
         sns.lineplot(x=pdf_mp.index, y=pdf_mp, label='MP pdf')
         sns.lineplot(x=pdf_kde.index, y=pdf_kde, label='KDE pdf')
@@ -26,7 +38,46 @@ class TEST_SignalEnhance(unittest.TestCase):
 
         self.assertTrue(np.sum(np.square(pdf_mp - pdf_kde)).squeeze() <= 1e-5)
 
-    def test_calibration(self):
-
-        eval_max, sigma = self._enhancer.calibrate()
+        eval_max, sigma = enhancer._fit_max_eval()
         self.assertAlmostEqual(sigma, 1, places=4)
+
+        enhancer.denoise(method='Constant Residual')
+        enhancer.denoise(method='Target Shrinkage', alpha=0)
+
+    def test_matrix_with_signal(self):
+
+        alpha, n_cols, n_factor, q = 0.995, 1000, 100, 10
+        cov = np.cov(np.random.normal(size=(n_cols * q, n_cols)), rowvar=False)
+        cov = alpha * cov + (1 - alpha) * self._get_random_cov(n_cols, n_factor)
+        corr = SignalEnhance.cov_to_corr(cov)
+
+        enhancer = SignalEnhance(matrix=cov, is_cov=True, q=q, band_width=0.01)
+        _, sigma = enhancer._fit_max_eval()
+
+        eval, evec = enhancer.get_sorted_pca(corr)
+        pdf_mp = enhancer._marcenko_pastur_pdf(sigma=sigma)
+        pdf_kde = enhancer._fit_empirical_kde(lambds=np.diag(eval))
+
+        sns.lineplot(x=pdf_mp.index, y=pdf_mp, label='MP pdf')
+        sns.lineplot(x=pdf_kde.index, y=pdf_kde, label='KDE pdf')
+        plt.show()
+        plt.close()
+
+        original_eval = enhancer._original_corr_eval
+        original_eval = np.diag(original_eval)
+
+        enhancer.denoise(method='Constant Residual')
+        denoised_eval, _ = SignalEnhance.get_sorted_pca(enhancer._denoised_corr)
+        denoised_eval = np.diag(denoised_eval)
+        sns.lineplot(x=range(len(original_eval)), y=original_eval, label='Before denoise')
+        sns.lineplot(x=range(len(denoised_eval)), y=denoised_eval, label='After denoise', linestyle='--')
+        plt.show()
+        plt.close()
+
+        enhancer.denoise(method='Target Shrinkage', alpha=0)
+        denoised_eval, _ = SignalEnhance.get_sorted_pca(enhancer._denoised_corr)
+        denoised_eval = np.diag(denoised_eval)
+        sns.lineplot(x=range(len(original_eval)), y=original_eval, label='Before denoise')
+        sns.lineplot(x=range(len(denoised_eval)), y=denoised_eval, label='After denoise', linestyle='--')
+        plt.show()
+        plt.close()
